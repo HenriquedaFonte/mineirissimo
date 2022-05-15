e3761a023d08d4991a65c7198338913a456aa0e3
henriquedafonte <henriquegdafonte@gmail.com>
2022-05-14 20:50:39 -0400

    Correcao alt das fotos

--- javascript.js
+++ javascript.js
@@ -2,31 +2,7 @@ window.addEventListener('scroll', onScroll);
 
 onScroll();
 
-$('.carousel').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-
-
-    
+   
 
 function onScroll() {
   showNavOnScroll();
@@ -152,5 +128,26 @@ ScrollReveal({
 #innovation .col-e,
 #testimonials`);
 
+$('.carousel').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
 
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
