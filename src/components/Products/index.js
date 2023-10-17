90154406c308547af13893ca002dbccf89b7f082
João Maia <joaocarlosbgm@gmail.com>
2023-10-17 17:49:46 -0300

    feat:presentation in products section

---
+++ src/components/Products/index.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+import ilustrationProducts from "../../assets/images/ilustration-products.png";
+
+import styles from "./products.module.scss";
+
+export function Products() {
+  return (
+    <section className={styles.products} id="produtos">
+      <h4 className={styles.title}>Produtos</h4>
+      <h1 className={styles.headline}>
+        Apresentamos alguns de nosso produtos mais vendidos
+      </h1>
+
+      <div className={styles.presentation}>
+        <div className={styles.content}>
+          <h3 className={styles.contentTitle}>
+            Nossa principal estrela, o pão de queijo!
+          </h3>
+          <p className={styles.paragraph}>
+            A receita é um segredinho que nossas avós já sabiam. Manuseavam
+            corretamente os ingredientes em porções generosas que, levadas ao
+            forno, recebiam o toque mágico da transformação. Retirados, já
+            crescidos, cheirosos e quentinhos, eram servidos com um delicioso
+            café. O que procuramos fazer, é um resgate dessa época. Um tempo que
+            não faz tanto tempo assim. E o principal, fazemos com todo
+            carinho,amor e alegria, como nossas vovós nos ensinaram. Como{" "}
+            <span>TODOS</span> os nossos produtos, nosso pão de queijo não leva
+            farinha e é <span>100% SEM GLÚTEM.</span>
+          </p>
+        </div>
+        <div>
+          <Image
+            className={styles.contentImage}
+            src={ilustrationProducts}
+            alt="Imagem com dois pães de queijos em cima de um prato"
+          />
+        </div>
+      </div>
+
+      <div className={styles.productsCards}>
+        colocar o componente de card dos produtos
+      </div>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import { Header } from "components/Header";
 import { Hero } from "components/Hero";
+import { Products } from "components/Products";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <Products />
       </main>
     </>
   );
