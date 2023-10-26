cb76edb8a183258e5913472ba684270acee3afcc
João Maia <joaocarlosbgm@gmail.com>
2023-10-26 11:32:03 -0300

    remove list scroll function

--- src/utils/listScroll/index.js
+++
@@ -1,9 +0,0 @@
-export function scrollLeftProducts() {
-  const left = document.querySelector("#productList");
-  left?.scrollBy(-760, 0);
-}
-
-export function scrollRightProducts() {
-  const right = document.querySelector("#productList");
-  right?.scrollBy(760, 0);
-}
