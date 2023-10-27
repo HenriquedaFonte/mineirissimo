5e7c700d63935f6b47c4795f70e9b604e4552661
João Maia <joaocarlosbgm@gmail.com>
2023-10-27 17:24:57 -0300

    feat: testimonial section for desktop

---
+++ src/database/testimonials/index.js
@@ -0,0 +1,51 @@
+import userImg from "../../assets/icons/user-img.jpg";
+
+export const testimonialsData = [
+  {
+    id: 1,
+    name: "Carolina da Fonte",
+    message:
+      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
+    img: userImg,
+  },
+  {
+    id: 2,
+    name: "Marta Cavalcanti",
+    message:
+      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
+
+    img: userImg,
+  },
+  {
+    id: 3,
+    name: "Sol Guimarães",
+    message:
+      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
+
+    img: userImg,
+  },
+  {
+    id: 4,
+    name: "Ana Luiza",
+    message:
+      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
+
+    img: userImg,
+  },
+  {
+    id: 5,
+    name: "João Maia",
+    message:
+      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
+
+    img: userImg,
+  },
+  {
+    id: 6,
+    name: "Luis da Fonte",
+    message:
+      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
+
+    img: userImg,
+  },
+];
