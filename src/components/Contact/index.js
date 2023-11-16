d423a7e9f4a70e79d5ce0ff62f3117e2d0b8e2a1
João Maia <joaocarlosbgm@gmail.com>
2023-11-16 09:54:50 -0300

    feat: contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,8 @@
 import Link from "next/link";
-import {
-  IoLocationOutline,
-  IoMailOutline,
-  IoLogoInstagram,
-} from "react-icons/io5";
+import Image from "next/image";
+
+import Logo from "../../assets/icons/logo.png";
+
 import { BsWhatsapp } from "react-icons/bs";
 
 import { PrimaryButton } from "components/Buttons";
@@ -15,23 +14,16 @@ export function Contact() {
     <section className={styles.contact} id="contato">
       <div className={styles.container}>
         <div className={styles.content}>
-          <header>
-            <h2 className={styles.title}>Entre em contato com a gente!</h2>
-          </header>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <IoLocationOutline />
-              R. Sergio Magalhães, 54 Graças Recife-PE
-            </li>
-            <li className={styles.item}>
-              <IoMailOutline />
-              mineirissimoartesanal@gmail.com
-            </li>
-            <li className={styles.item}>
-              <IoLogoInstagram />
-              mineirissimo.recife
-            </li>
-          </ul>
+          <div className={styles.logoContainer}>
+            <Image
+              className={styles.logo}
+              src={Logo}
+              alt="Logomarca da Mineiríssimo"
+            />
+          </div>
+          <h2 className={styles.title}>
+            Sabores únicos para momentos especiais.
+          </h2>
           <Link
             href="https://web.whatsapp.com/send?phone=5581996272423"
             target="_blank"
@@ -48,7 +40,6 @@ export function Contact() {
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.556599068668!2d-34.902991249168416!3d-8.044564182544107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab18fb5fce8337%3A0xb27923733205c42e!2sMineir%C3%ADssimo!5e0!3m2!1spt-BR!2sca!4v1652234316823!5m2!1spt-BR!2sca"
               allowFullScreen=""
-              loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
