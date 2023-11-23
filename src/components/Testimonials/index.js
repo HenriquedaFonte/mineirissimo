083de768bd1d4beb828f4b0e2a70bd9451f29e84
João Maia <joaocarlosbgm@gmail.com>
2023-11-23 16:34:32 -0300

    feat: motion on testimonials section

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,18 +1,31 @@
 import { Swiper } from "swiper/react";
+import { motion } from "framer-motion";
 
 import { TestimonialsCards } from "components/TestimonialsCards";
 
 import styles from "./testimonials.module.scss";
 
+const parent = {
+  hidden: { y: 100, opacity: 0 },
+  visible: { y: 0, opacity: 1, transition: { duration: 1 } },
+};
+
 export function Testimonials() {
   return (
-    <section className={styles.testimonials} id="depoimentos">
+    <motion.section
+      className={styles.testimonials}
+      id="depoimentos"
+      variants={parent}
+      initial="hidden"
+      whileInView="visible"
+      onReset={true}
+    >
       <h4 className={styles.title}>depoimentos</h4>
       <h1 className={styles.headline}>Veja quem já passou por aqui</h1>
 
       <Swiper className={styles.testimonialsList} id="testimonialsList">
         <TestimonialsCards />
       </Swiper>
-    </section>
+    </motion.section>
   );
 }
