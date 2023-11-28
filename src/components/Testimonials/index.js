542161301b8f54f10c1567146570fd45041357af
João Maia <joaocarlosbgm@gmail.com>
2023-11-28 12:33:08 -0300

    fix: framer motion in testimonials component

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,32 +1,25 @@
 import { Swiper } from "swiper/react";
-import { motion } from "framer-motion";
 
 import { TestimonialsCards } from "components/TestimonialsCards";
+import { AnimatedElement } from "utils/animations";
 
 import styles from "./testimonials.module.scss";
 
-const parent = {
-  hidden: { y: 100, opacity: 0 },
-  visible: { y: 0, opacity: 1, transition: { duration: 1 } },
-};
-
 export function Testimonials() {
   return (
-    <motion.section
+    <AnimatedElement
+      element="section"
       className={styles.testimonials}
       id="depoimentos"
-      variants={parent}
-      initial="hidden"
-      whileInView="visible"
     >
       <h4 className={styles.title}>depoimentos</h4>
       <h1 className={styles.headline}>Veja quem já passou por aqui</h1>
 
-      <motion.div variants={parent} initial="hidden" whileInView="visible">
+      <AnimatedElement element="div">
         <Swiper className={styles.testimonialsList} id="testimonialsList">
           <TestimonialsCards />
         </Swiper>
-      </motion.div>
-    </motion.section>
+      </AnimatedElement>
+    </AnimatedElement>
   );
 }
