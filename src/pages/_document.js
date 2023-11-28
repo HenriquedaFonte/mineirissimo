a65c93a32f282edab01726fd1a6cc7786427ab54
João Maia <joaocarlosbgm@gmail.com>
2023-11-28 12:40:35 -0300

    merge branch next into bg-22-cookies

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,7 +15,11 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
