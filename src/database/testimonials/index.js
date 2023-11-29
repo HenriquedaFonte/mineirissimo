78de6ef7f69265a61fdf767d4857ffc7fb8c6680
João Maia <joaocarlosbgm@gmail.com>
2023-11-29 17:45:15 -0300

    fix: deleting users images and remove property img from testimonial database

--- src/database/testimonials/index.js
+++ src/database/testimonials/index.js
@@ -1,51 +1,38 @@
-import userCarolImg from "../../assets/icons/carol.png";
-import userAnaImg from "../../assets/icons/ana.png";
-import userMartaImg from "../../assets/icons/marta.png";
-import userJoaoImg from "../../assets/icons/joao.png";
-import userSolImg from "../../assets/icons/sol.png";
-import userLuisImg from "../../assets/icons/luis.png";
-
 export const testimonialsData = [
   {
     id: 1,
     name: "Carolina da Fonte",
     message:
       "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineiríssimo.",
-    img: userCarolImg,
   },
   {
     id: 2,
     name: "Marta Cavalcanti",
     message:
       "A qualidade desses produtos é incomparável. Meu filho sempre leva de lanche para o colégio os pães de queijo, a coxinha e a pizza, amo!",
-    img: userMartaImg,
   },
   {
     id: 3,
     name: "Sol Guimarães",
     message:
       "A coxinha de pão de queijo é uma invenção genial! E a pizza de pão de queijo? Uma verdadeira surpresa de sabores deliciosos!",
-    img: userSolImg,
   },
   {
     id: 4,
     name: "Ana Luiza",
     message:
       "O pão de queijo do reino é a combinação perfeita de tradição e sabor. Sem falar na crostata, que é irresistível! Recomendo muito!",
-    img: userAnaImg,
   },
   {
     id: 5,
     name: "Lorena Maia",
     message:
       "Sou do Rio, mas sempre que vou a Recife tenho que passar aqui pra repor meu estoque! Levo no avião e consigo aproveitar um bom tempo esse gostinho!!!",
-    img: userJoaoImg,
   },
   {
     id: 6,
     name: "Luis da Fonte",
     message:
       "Pão de queijo mineiro excelente. Variedade no cardápio e opções de pizza de pão de queijo e crostata de pão de queijo.",
-    img: userLuisImg,
   },
 ];
