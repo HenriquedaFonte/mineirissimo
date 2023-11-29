6fa57dcbdf4ce8a1aec9b0675544f35d358eb3d5
João Maia <joaocarlosbgm@gmail.com>
2023-11-29 16:59:40 -0300

    fix: remove images from testimonials and feature real testimonials

--- src/database/testimonials/index.js
+++ src/database/testimonials/index.js
@@ -17,40 +17,35 @@ export const testimonialsData = [
     id: 2,
     name: "Marta Cavalcanti",
     message:
-      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
-
+      "A qualidade desses produtos é incomparável. Meu filho sempre leva de lanche para o colégio os pães de queijos, a coxinha e a pizza, amo!",
     img: userMartaImg,
   },
   {
     id: 3,
     name: "Sol Guimarães",
     message:
-      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
-
+      "A coxinha de pão de queijo é uma invenção genial! E a pizza de pão de queijo? Uma verdadeira surpresa de sabores deliciosos!",
     img: userSolImg,
   },
   {
     id: 4,
     name: "Ana Luiza",
     message:
-      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
-
+      "O pão de queijo do reino é a combinação perfeita de tradição e sabor. Sem falar na crostata, que é irresistível! Recomendo muito!",
     img: userAnaImg,
   },
   {
     id: 5,
     name: "João Maia",
     message:
-      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
-
+      "Amo os produtos dessa loja! O pão de queijo é sensacional, a coxinha é incrível, e a pizza é a melhor que existe. Simplesmente perfeito!",
     img: userJoaoImg,
   },
   {
     id: 6,
     name: "Luis da Fonte",
     message:
-      "O melhor pão de queijo que comi na vida, sem falar na coxinha, maravilhosa, o lanche do fim de semana sempre tem Mineirissimo.",
-
+      "A variedade é incrível! Pão de queijo, coxinha, e a pizza... simplesmente espetaculares e ainda tem muitas outras coisas. Lugar top!",
     img: userLuisImg,
   },
 ];
